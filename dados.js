3df102b84a8ac1c3f4b7fbc44bedb7df7286d4b5
wellygtonDev2024 <wellygton.ttrainer@gmail.com>
2024-09-06 14:59:54 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "O que é HTML?",
+        descricao: "Por ser uma tecnologia utilizada para desenvolver sites, é comum pensarmos que programamos em HTML. Uma observação importante é que o HTML não é uma linguagem de programação, e sim de marcação. Mas por que o HTML não é uma...",
+        link: "https://www.alura.com.br/artigos/html",
+        tags: "html html5"
+    },
+    {
+        titulo: "O que é CSS",
+        descricao: "Olá, pessoa incrível! Hoje vamos abordar um tema que considero fundamental para qualquer pessoa que esteja na área de desenvolvimento web, ou até mesmo para aquelas que estão pensando em entrar nesse maravilhoso mundo. Vamos falar de CSS — Cascading Style Sheets. Prepare-se para mergulhar nessa aventura!",
+        link: "https://www.alura.com.br/artigos/css",
+        tags: "css estilização estilo"
+    },
+    {
+        titulo: "O que é JavaScript",
+        descricao: "Quando você está pesquisando sobre o mercado de programação é muito comum se deparar com o nome JavaScript. E não é à toa, pois essa linguagem está presente em toda a Web e você provavelmente lida com ela diariamente enquanto usa seu navegador (e até mesmo fora dele!).Vamos descobrir nesse artigo o que é JS e o que faz o JavaScript ser uma linguagem tão relevante e tão utilizada atualmente!",
+        link: "https://www.alura.com.br/artigos/javascript",
+        tags: "javascript JavaScript programação Programação linguagem de programação java script"
+    }
+];
